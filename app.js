1f86b325f0f901e32b71d4dd354c52400812ef1b
elnuraErkinbekova <elnuraerkinbekova8@gmail.com>
2025-10-16 16:52:46 +0600

    Add js

---
+++ app.js
@@ -0,0 +1,37 @@
+const inputValue = document.getElementById("user-input");
+
+const number = document.querySelectorAll(".numbers").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    if (inputValue.innerText === "NaN" || inputValue.innerText === "0") {
+      inputValue.innerText = "";
+    }
+    inputValue.innerText += e.target.innerHTML.trim();
+  });
+});
+
+
+const calculate = document.querySelectorAll(".operations").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    const operation = e.target.innerHTML;
+    let lastValue = inputValue.innerText.substring(inputValue.innerText.length - 1);
+
+    if (operation === "=") {
+      try {
+        inputValue.innerText = eval(inputValue.innerText);
+      } catch (error) {
+        inputValue.innerText = "Error";
+      }
+    } else if (operation === "AC") {
+      inputValue.innerText = "0";
+    } else if (operation === "DEL") {
+      inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
+      if (inputValue.innerText.length === 0) {
+        inputValue.innerText = "0";
+      }
+    } else {
+      if (!isNaN(parseInt(lastValue)) || lastValue === '.') {
+        inputValue.innerText += operation;
+      }
+    }
+  });
+});
